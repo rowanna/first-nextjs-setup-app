117d1b911f077a62c97cd04e1cc1783059046cef
rowanna <rowankimna@gmail.com>
2025-02-03 12:02:19 +0900

    feat: dashboard 라우터 생성

---
+++ src/app/dashboard/customers/page.tsx
@@ -0,0 +1,16 @@
+import { notFound } from "next/navigation";
+
+export default function Customers() {
+  const handleError = () => {
+    throw new Error("일부러 에러 발생");
+  };
+  try {
+    handleError();
+  } catch (error) {
+    console.log(process.env.NODE_ENV, "===");
+
+    notFound();
+  }
+
+  return <div>customer page</div>;
+}
